84e525f8dc07511b5de7a958a2a7b282ed9432f5
Guntak Kim <caputdraconis@kakao.com>
2022-10-15 18:14:10 +0900

    fix: Module Not Found Error Fix

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import nodemailer from "nodemailer";
 import AWS from "aws-sdk";
 
-// AWS Configuration(Access Key, Secret Key, etc)
-AWS.config.loadFromPath(__dirname + "/aws_config.json");
+// AWS 설정 - 설정에 필요한 민감한 Accesskey, secretAccessKey, region을 .env 파일에 보관하였습니다.
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
+// nodemailer transport 생성
 const transporter = nodemailer.createTransport({
   SES: new AWS.SES({
     apiVersion: "2010-12-01",
@@ -15,7 +20,7 @@ transporter.sendMail(
     from: "noreply@caput.dev",
     to: "caputdraconis@kakao.com",
     subject: "Test Email",
-    html: "<h1> Welcome to register our website! </h1><br><a href='{Email Address Verify API}'>Verify My Email Address</a>",
+    html: "<h1> Welcome to our website! </h1><br><a href='{Email Address Verify API}'>Verify My Email Address</a>",
   },
   (err, info) => {
     if (err) {
